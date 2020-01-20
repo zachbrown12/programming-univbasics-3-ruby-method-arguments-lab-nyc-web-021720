5e1591d967de209c74e25726308c6854dee79821
Zachary Brown <zachmb22@gmail.com>
2020-01-20 04:20:39 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -10,7 +10,15 @@ def introduction(name)
 end
 
 def introduction_with_language(name, language)
-  puts ""
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+def introduction_with_language_optional(name, language="Ruby")
+ puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+
+  
 
 
 
